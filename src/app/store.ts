80159a3d19f4c86dfb911a687a5137b82f2b60d6
Adriel Klein <adrielmklein@gmail.com>
2023-02-24 10:45:25 -0600

    build out kino slice of redux

--- src/app/store.ts
+++ src/app/store.ts
@@ -1,9 +1,11 @@
 import { configureStore, ThunkAction, Action } from '@reduxjs/toolkit';
 import counterReducer from '../features/counter/counterSlice';
+import kinoReducer from '../features/kino/kinoSlice';
 
 export const store = configureStore({
   reducer: {
     counter: counterReducer,
+    kino: kinoReducer,
   },
 });
 
