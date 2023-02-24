7c775b2d5535e19fac673cad019c596809e60f28
Adriel Klein <adrielmklein@gmail.com>
2023-02-24 15:27:06 -0600

    make app infinitely scrollable

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,15 @@
-import { useEffect, useMemo } from "react";
+import { useCallback, useEffect, useMemo, useRef } from "react";
 import "./App.css";
 import { useAppDispatch, useAppSelector } from "./app/hooks";
 import Game from "./features/kino/Game";
 import {
   fetchGamesAsync,
   selectGames,
+  selectMinFetchedGames,
   selectOldestDrawId,
   selectStatus,
+  increaseMinFetchedGames,
+  INITIAL_MIN_FETCHED_GAMES,
 } from "./features/kino/kinoSlice";
 import "bootstrap/dist/css/bootstrap.min.css";
 import Spinner from "react-bootstrap/Spinner";
@@ -18,33 +21,52 @@ const LoadingState = () => (
 );
 
 function App() {
+  const scrollRef = useRef<HTMLDivElement>(null);
   const dispatch = useAppDispatch();
   const games = useAppSelector(selectGames);
   const status = useAppSelector(selectStatus);
   const oldestDrawId = useAppSelector(selectOldestDrawId);
+  const minFetchedGames = useAppSelector(selectMinFetchedGames);
   const numFetchedGames = useMemo(() => games.length, [games]);
   const isInitialLoadingState = useMemo(
-    () => status === "loading" && numFetchedGames === 0,
+    () => numFetchedGames < INITIAL_MIN_FETCHED_GAMES,
     [status, numFetchedGames]
   );
 
+  const handleScroll = useCallback(() => {
+    console.log("handing scroll");
+    const { scrollTop, scrollHeight, clientHeight } = scrollRef.current!;
+
+    if (scrollTop + clientHeight === scrollHeight) {
+      console.log("User has scrolled to the bottom of the div!");
+      dispatch(increaseMinFetchedGames());
+    }
+  }, [dispatch, scrollRef]);
+
   useEffect(() => {
-    if (numFetchedGames < 15) {
+    if (numFetchedGames < minFetchedGames) {
       dispatch(fetchGamesAsync(oldestDrawId));
     }
-  }, [dispatch, numFetchedGames, oldestDrawId]);
+  }, [dispatch, minFetchedGames, oldestDrawId]);
 
   if (status === "failed") {
-    return <div>Failed to load data. Please refresh app or contact support.</div>;
+    return (
+      <div>Failed to load data. Please refresh app or contact support.</div>
+    );
   }
 
   return (
-    <div>
+    <div className="App">
       <div className="AppHeader">KINO</div>
       {isInitialLoadingState ? (
         <LoadingState />
       ) : (
-        <div className="Content ContentColumns">
+        <div
+          className="Content ContentColumns"
+          ref={scrollRef}
+          onScroll={handleScroll}
+          style={{ height: "100vh", overflow: "auto" }}
+        >
           {games.map((game) => (
             <Game key={game.gameNumber} {...game} />
           ))}
